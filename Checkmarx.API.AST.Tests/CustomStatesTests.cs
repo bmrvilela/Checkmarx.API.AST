6e3d4b1b9d2f4a61aa537e94fe83f7305407de28
bvilela <bruno.vilela@checkmarx.com>
2025-05-30 16:12:51 +0100

    Custom states API and Recalculate sast result predicate added

---
+++ Checkmarx.API.AST.Tests/CustomStatesTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Checkmarx.API.AST.Tests
+{
+    [TestClass]
+    public class CustomStatesTests
+    {
+
+        private static ASTClient astclient;
+
+        public static IConfigurationRoot Configuration { get; private set; }
+
+
+        [ClassInitialize]
+        public static void InitializeTest(TestContext testContext)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<ProjectTests>();
+
+            Configuration = builder.Build();
+
+            if (!string.IsNullOrWhiteSpace(Configuration["API_KEY"]))
+            {
+                astclient = new ASTClient(
+                new System.Uri(Configuration["ASTServer"]),
+                new System.Uri(Configuration["AccessControlServer"]),
+                Configuration["Tenant"],
+                Configuration["API_KEY"]);
+            }
+            else
+            {
+                astclient = new ASTClient(
+                new System.Uri(Configuration["ASTServer"]),
+                new System.Uri(Configuration["AccessControlServer"]),
+                Configuration["Tenant"],
+                Configuration["ClientId"],
+                Configuration["ClientSecret"]);
+            }
+
+        }
+
+        [TestMethod]
+        public void ListCustomStatesTest()
+        {
+            var customStates = astclient.GetAllCustomStates();
+            foreach (var item in customStates)
+                Trace.WriteLine($"Custom State: {item.Name} ({item.Id})");
+        }
+
+        [TestMethod]
+        public void CreateCustomStateTest()
+        {
+            astclient.CreateNewCustomState("NewStateToDelete");
+        }
+
+        [TestMethod]
+        public void DeleteCustomStateTest()
+        {
+            astclient.DeleteCustomState("NewStateToDelete");
+        }
+    }
+}
