4a515cb466c189e7e37d301a47eb4dcb0d0fcca7
Pedro Portilha <pedro.portilha@checkmarx.com>
2025-07-11 11:06:32 -0700

    Added the TooManyRequest to the list of responses it will trigger the retry.

--- Checkmarx.API.AST/ASTClient.cs
+++ Checkmarx.API.AST/ASTClient.cs
@@ -125,6 +125,7 @@ namespace Checkmarx.API.AST
 
         internal static readonly IAsyncPolicy<HttpResponseMessage> _retryPolicy = HttpPolicyExtensions
                                 .HandleTransientHttpError()
+                                .OrResult(response => response.StatusCode == System.Net.HttpStatusCode.TooManyRequests) // Specifically handle 429
                                 .WaitAndRetryAsync(10, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
                                 (exception, timeSpan, retryCount, context) =>
                                 {
